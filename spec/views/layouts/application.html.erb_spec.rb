3c9960d2add026af12603fcac074c96e08b3012e
Chylli <chylli.email@gmail.com>
2014-06-25 00:26:53 +0800

    change something about ui

--- spec/views/layouts/application.html.erb_spec.rb
+++ spec/views/layouts/application.html.erb_spec.rb
@@ -1,9 +1,11 @@
-require 'spec_helper'
+require 'rails_helper'
 
 describe "layouts/application", :type => :view do
   it "show user name and not show Signin & Signup after signin" do
     assign(:user, double("User", :name => "user1"))
     render
+    assert_select "a[href=?]", user_path
+    assert_select "a.navbar-brand[href=?]", root_path, 1
     expect(rendered).to match(/user1/)
     expect(rendered).not_to match(/Signin/)
     expect(rendered).not_to match(/Signup/)
