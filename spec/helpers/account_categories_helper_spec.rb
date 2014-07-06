d56c9fca46cb379910c999f796a5fb757f9ba0db
Chylli <chylli.email@gmail.com>
2014-07-06 12:33:51 +0800

    add todo

--- spec/helpers/account_categories_helper_spec.rb
+++ spec/helpers/account_categories_helper_spec.rb
@@ -3,6 +3,7 @@ require 'rails_helper'
 describe AccountCategoriesHelper, :type => :helper do
   it 'should process nested categories to a html string' do
     user1 = create(:user1)
+    #TODO use assert_select to test
     expect(helper.nested_account_categories(user1.account_categories.arrange)).to match(/class="nested_categories"/)
   end
 end
