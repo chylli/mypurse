91d79fd4ba466143c191a8330872bcea85663c02
Chylli <chylli.email@gmail.com>
2014-07-07 17:45:32 +0800

    fix test view i18n problme

--- spec/controllers/application_controller_spec.rb
+++ spec/controllers/application_controller_spec.rb
@@ -22,6 +22,15 @@ RSpec.describe ApplicationController, :type => :controller do
       expect(assigns(:user)).to eq(user)
     end
 
+    #TODO this test should be before en test
+    # because if not then the i18n will affetc other view tests
+    it "will set I18n.locale to zh when user language is zh" do
+      user.language = 'zh'
+      user.save
+      get :index, {}, {user_id: user.id}
+      expect(I18n.locale).to eq(:zh)
+    end
+
     it "will set I18n.locale to en when user language is en" do
       user.language = 'en'
       user.save
@@ -29,12 +38,6 @@ RSpec.describe ApplicationController, :type => :controller do
       expect(I18n.locale).to eq(:en)
     end
 
-    it "will set I18n.locale to zh when user language is zh" do
-      user.language = 'zh'
-      user.save
-      get :index, {}, {user_id: user.id}
-      expect(I18n.locale).to eq(:zh)
-    end
   end
 end
 
