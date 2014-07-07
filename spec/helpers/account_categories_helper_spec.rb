318b9c810f820e8410c171ff7d181c242c9f45a3
Chylli <chylli.email@gmail.com>
2014-07-07 11:14:18 +0800

    test category helper with stub

--- spec/helpers/account_categories_helper_spec.rb
+++ spec/helpers/account_categories_helper_spec.rb
@@ -3,7 +3,9 @@ require 'rails_helper'
 describe AccountCategoriesHelper, :type => :helper do
   it 'should process nested categories to a html string' do
     user1 = create(:user1)
+    #helper.stub(:render).and_return("category_element")
+    allow(helper).to receive(:render).and_return("category_element")
     #TODO use assert_select to test
-    expect(helper.nested_account_categories(user1.account_categories.arrange)).to match(/class="nested_categories"/)
+    expect(helper.nested_account_categories(user1.account_categories.arrange)).to eq("category_element<div class=\"nested_categories\">category_element<div class=\"nested_categories\"></div></div>category_element<div class=\"nested_categories\">category_element<div class=\"nested_categories\">category_element<div class=\"nested_categories\"></div></div>category_element<div class=\"nested_categories\"></div></div>")
   end
 end
