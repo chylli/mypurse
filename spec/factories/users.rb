44b0ad48c746672bb5f1131d375cac0b6a6c24c1
Chylli <chylli.email@gmail.com>
2014-07-09 14:09:11 +0800

    fix bugs

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -11,16 +11,16 @@ FactoryGirl.define do
 
   factory :user1, class: User do
     id 1
-    name
-    email
+    name 'user1'
+    email 'user1@example.com'
     password "password1"
     password_confirmation "password1"
     language 'en'
   end
   factory :user2, class: User do
     id 2
-    name
-    email
+    name 'user2'
+    email 'user2@example.com'
     password "password2"
     password_confirmation "password2"
     language 'en'
