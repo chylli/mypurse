efd7ee447f8eb39b1b20586f3f367aa9fcd0074a
Chylli <chylli.email@gmail.com>
2014-07-09 14:02:24 +0800

    fix a bug

--- spec/views/configure/accounts.html.haml_spec.rb
+++ spec/views/configure/accounts.html.haml_spec.rb
@@ -4,6 +4,7 @@ describe 'configure/accounts.html.haml', type: :view do
   it 'should can be rendered' do
     user = create(:user1)
     assign(:user, user)
+    assign(:accounts, user.accounts)
     render
     expect(rendered).to include('Accounts')
   end
