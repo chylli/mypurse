b8e6db60b87a4a69986ae312fc021392eebc0ba1
Chylli <chylli.email@gmail.com>
2014-07-30 13:59:27 +0800

    fix migrations

--- db/migrate/20140630035248_add_default_account_type_to_account_category.rb
+++ db/migrate/20140630035248_add_default_account_type_to_account_category.rb
@@ -1,5 +1,5 @@
 class AddDefaultAccountTypeToAccountCategory < ActiveRecord::Migration
   def change
-    add_column :Categories, :default_account_type, :string
+    add_column :account_categories, :default_account_type, :string
   end
 end
